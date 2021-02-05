651eb38e443071f67a639a7777d30ca153c038ff
DanielFilho2000 <daniel.agff@gmail.com>
2021-02-05 13:18:52 -0300

    05/02/2021

--- src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -22,11 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/usuarios")
 @CrossOrigin("*")
-public class UsuarioController {
+public class UsuarioController 
+{	
 
 	@Autowired
 	private UsuarioRepository repository;
-
+	
 	@Autowired
 	private UsuarioService usuarioService;
 
@@ -46,17 +47,11 @@ public class UsuarioController {
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 
+
 	@PostMapping("/cadastrar")
-	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) 
-	{
-		Optional<Usuario> user = usuarioService.CadastrarUsuario(usuario);
-		try {
-				return ResponseEntity.ok(user.get());
-		} catch (Exception e) 
-		{
-			return ResponseEntity.badRequest().build();
-		}
-		
+	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(usuarioService.CadastrarUsuario(usuario));
 	}
 
 	@PutMapping
--- src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -13,23 +13,19 @@ import org.springframework.stereotype.Service;
 
 
 @Service
-public class UsuarioService {
+public class UsuarioService 
+{
 
 	@Autowired
 	private UsuarioRepository repository;
 
-	public Optional<Usuario> CadastrarUsuario(Usuario usuario) {
-		
-		
-		if(repository.findByUsuario(usuario.getUsuario()).isPresent())
-			return null;
-		
+	public Usuario CadastrarUsuario(Usuario usuario) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
 		String senhaEncoder = encoder.encode(usuario.getSenha());
 		usuario.setSenha(senhaEncoder);
 
-		return Optional.of(repository.save(usuario));
+		return repository.save(usuario);
 	}
 
 	public Optional<UserLogin> Logar(Optional<UserLogin> user) {
@@ -46,13 +42,13 @@ public class UsuarioService {
 
 				user.get().setToken(authHeader);				
 				user.get().setNome(usuario.get().getNome());
-				user.get().setSenha(usuario.get().getSenha());
-				
+
 				return user;
 
 			}
 		}
 		return null;
 	}
+	
 
 }
