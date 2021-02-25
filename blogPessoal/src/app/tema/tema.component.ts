0762b494485942b2998b492c675456066dd576a4
DanielFilho2000 <daniel.agff@gmail.com>
2021-02-25 10:59:11 -0300

    Post e Delete Completos

--- blogPessoal/src/app/tema/tema.component.ts
+++ blogPessoal/src/app/tema/tema.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaComponent implements OnInit {
 
-  constructor() { }
+  tema : Tema = new Tema()
+  listaTemas : Tema[]   
 
-  ngOnInit(): void {
+  constructor(
+    private router : Router,
+    private temaService: TemaService
+  ) { }
+
+  ngOnInit() {
+    if(environment.token == '')
+    {
+      this.router.navigate(['/entrar'])
+    }
+    this.findAllTemas()
   }
+
+  findAllTemas()
+  {
+    this.temaService.pegaTudoPeloTema().subscribe((resp : Tema[])=>
+    {
+      this.listaTemas = resp
+    })
+  }
+  
+  cadastrarTema()
+  {
+    this.temaService.postTema(this.tema).subscribe((resp:Tema) =>{
+      this.tema = resp
+      alert('Tema Cadastrado !!!')
+      this.findAllTemas()
+      this.tema = new Tema()  
+      
+    })
+  }
+
 
 }
